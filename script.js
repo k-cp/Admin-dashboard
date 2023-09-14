0bfd0753375bb143905b605c4b9f22501d4a3d90
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-09-14 19:40:44 +0200

    Fix

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const initialScreenWidth = window.innerWidth;
-
-const element = document.querySelector('.side-bar');
-
-const container = document.querySelector('.container')
-
-element.style.width = `${initialScreenWidth * 0.2}px`; // Adjust the factor as needed
-
-container.style.gridTemplateColumns = element.style.width + ' auto';
+function switchNav() {
+    var side = document.querySelector('.side-bar')
+    var main = document.getElementById('main')
+    side.classList.toggle('closed')
+    if (side.classList.contains('closed')) {
+        side.style.width = '85px';
+        main.style.marginLeft = '85px';
+        document.querySelector('.logo-name').style.opacity = '0';
+        option = document.querySelectorAll('.option-name');
+        optionLink = document.querySelectorAll('.option-link');
+        option.forEach(element => {
+            element.style.opacity = '0'
+            element.style.width = '0'
+        });
+        optionLink.forEach(element => {
+            element.style.gap = '0'
+        });
+    }
+    else {
+        side.style.width = '250px';
+        main.style.marginLeft = '250px';
+        option = document.querySelectorAll('.option-name');
+        optionLink = document.querySelectorAll('.option-link');
+        setTimeout(() => {
+            document.querySelector('.logo-name').style.opacity = '100';
+            optionLink.forEach(element => {
+                element.style.gap = '15px'
+            });
+            option.forEach(element => {
+                element.style.opacity = '100'
+        })
+        }, 200)
+}
+}
